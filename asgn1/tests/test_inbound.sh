9714ca82c8111bc56f8533cc48fa1e51f8e0f3aa
Max Ratcliff <mwratcli@ucsc.edu>
2024-04-13 01:32:24 +0000

    design typos, and one more test case

--- asgn1/tests/test_inbound.sh
+++ asgn1/tests/test_inbound.sh
@@ -82,7 +82,36 @@ fi
 rm out.txt
 rm ex_out.txt
 
-#test 4: 512 -512
+#test 4: -512 -512
+# Creates the example output
+echo -1024 > ex_out.txt
+# Runs the program
+./calc -512 -512 > out.txt 
+
+# Ensures exit code is Zero
+if [ $? -ne 0 ]; then
+    echo "invalid exit code" $?
+    rm out.txt
+    rm ex_out.txt
+    exit 1
+fi
+
+# Ensures differences *arent* found
+diff out.txt ex_out.txt
+if [ $? -ne 0 ]; then
+    echo "wrong output "
+    rm out.txt
+    rm ex_out.txt
+    exit 1
+fi
+
+# Cleans up files created
+rm out.txt
+rm ex_out.txt
+
+
+
+#test 5: 512 -512
 # Creates the example output
 echo 0 > ex_out.txt
 # Runs the program
@@ -110,7 +139,7 @@ rm out.txt
 rm ex_out.txt
 
 
-#test 5: 0 0
+#test 6: 0 0
 # Creates the example output
 echo 0 > ex_out.txt
 # Runs the program
@@ -138,7 +167,7 @@ rm out.txt
 rm ex_out.txt
 
 
-#test 6: +10 +10
+#test 7: +10 +10
 # Creates the example output
 echo 20 > ex_out.txt
 # Runs the program
