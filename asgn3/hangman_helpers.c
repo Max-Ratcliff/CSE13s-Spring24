2a1c6e321a6e35ae73109500b803957cdeded533
Max Ratcliff <mwratcli@ucsc.edu>
2024-04-26 22:57:40 +0000

    initial commit + working on design a little

---
+++ asgn3/hangman_helpers.c
@@ -0,0 +1,13 @@
+#include "hangman_helpers.h"
+
+bool is_lowercase_letter(char c) {
+}
+
+bool is_valid_secret(const char *secret) {
+}
+
+bool string_contains_character(const char *s, char c) {
+}
+
+char read_letter(void) {
+}
