046d4acd26a5aa5e8a192e7b6a68e32e6a3b0047
Max Ratcliff <mwratcli@ucsc.edu>
2024-05-21 00:15:11 +0000

    valgrind

--- asgn6/hash.h
+++ asgn6/hash.h
@@ -1,21 +1,21 @@
 #ifndef _HASH
 
-#define _HASH 
+#define _HASH
 
 #include "ll.h"
 
 typedef struct Hashtable Hashtable;
 
 struct Hashtable {
-	// your datastructure here; this is just a placeholder
-	int h;
+    // your datastructure here; this is just a placeholder
+    int h;
 };
 
-Hashtable * hash_create(void);
+Hashtable *hash_create(void);
 
 bool hash_put(Hashtable *, char *key, int val);
 
-int * hash_get(Hashtable *, char *key);
+int *hash_get(Hashtable *, char *key);
 
 void hash_destroy(Hashtable **);
 
--- asgn6/ll.c
+++ asgn6/ll.c
@@ -1,89 +1,87 @@
 #include "ll.h"
+
 #include <stdbool.h>
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-LL * list_create(void)
-{
-	LL *l = (LL *)malloc(sizeof(LL));
-	if (l == NULL) {
-		return NULL;
-	}
-	l->head = NULL;
-	return l;
+LL *list_create(void) {
+    LL *l = (LL *) malloc(sizeof(LL));
+    if (l == NULL) {
+        return NULL;
+    }
+    l->head = NULL;
+    return l;
 }
 
-bool list_add(LL *l, item *i)
-{
-	Node *n = (Node *)malloc(sizeof(Node));
-	if (n == NULL) {
-		return false;
-	}
-	n->data = *i;
-	n->next = NULL;
-	if (l->head == NULL) {
-		l->head = n;
-		return true;
-	} else {
-		Node *tail = l->head;
-		while (tail->next != NULL) {
-			tail = tail->next;
-		}
-		tail->next = n;
-		return true;
-	}
+bool list_add(LL *l, item *i) {
+    Node *n = (Node *) malloc(sizeof(Node));
+    if (n == NULL) {
+        return false;
+    }
+    n->data = *i;
+    n->next = NULL;
+    if (l->head == NULL) {
+        l->head = n;
+        return true;
+    } else {
+        Node *tail = l->head;
+        while (tail->next != NULL) {
+            tail = tail->next;
+        }
+        tail->next = n;
+        return true;
+    }
 }
 
-
-item * list_find(LL *l, bool (*cmpfn)(item *,item *), item *i)
-{
-	Node *n = l->head;
-        while (n != NULL) {
-		if (cmpfn(&n->data, i)) {
-			return &n->data;
-		}
-		n = n->next;
-	}
-	return NULL;
+item *list_find(LL *l, bool (*cmpfn)(item *, item *), item *i) {
+    Node *n = l->head;
+    while (n != NULL) {
+        if (cmpfn(&n->data, i)) {
+            return &n->data;
+        }
+        n = n->next;
+    }
+    return NULL;
 }
 
-void list_destroy(LL** ll){
-	if(ll == NULL || *ll == NULL){return;}
-	Node *curr = (*ll)->head;
-	Node *next_node = NULL;
-	while(curr != NULL){
-		next_node = curr->next;
-		free(curr);
-		curr = next_node;
-	}
-	(*ll)->head = NULL;
-	free(*ll);
-	*ll = NULL;
+void list_destroy(LL **ll) {
+    if (ll == NULL || *ll == NULL) {
+        return;
+    }
+    Node *curr = (*ll)->head;
+    Node *next_node = NULL;
+    while (curr != NULL) {
+        next_node = curr->next;
+        free(curr);
+        curr = next_node;
+    }
+    (*ll)->head = NULL;
+    free(*ll);
+    //*ll = NULL;
 
-	return;
+    return;
 }
-void list_remove(LL* ll, bool(*cmpfn)(item *, item *), item* iptr){
-	if(ll->head == NULL){// handles no head
-		return;
-	}
-	
-	Node *curr = ll->head; //sets current pointer to the start of the linked list
-	Node *prev = NULL; 
-	if (cmpfn(&curr->data, iptr)) {//handles head being deleted
-		ll->head = curr->next;//sets new head to 2nd item of list	
-		free(curr);
-		return;
-	}
+void list_remove(LL *ll, bool (*cmpfn)(item *, item *), item *iptr) {
+    if (ll->head == NULL) { // handles no head
+        return;
+    }
 
-        while (curr != NULL) {
-		if (cmpfn(&curr->data, iptr)) {// finds target item in list
-			prev->next = curr->next; //deletes item
-			free(curr);	//free memory
-		}
-		prev = curr; //increment to look at the next item in the list
-		curr = curr->next;
-	}
-	return;
+    Node *curr = ll->head; //sets current pointer to the start of the linked list
+    Node *prev = NULL;
+    if (cmpfn(&curr->data, iptr)) { //handles head being deleted
+        ll->head = curr->next; //sets new head to 2nd item of list
+        free(curr);
+        return;
+    }
+
+    while (curr != NULL) {
+        if (cmpfn(&curr->data, iptr)) { // finds target item in list
+            prev->next = curr->next; //deletes item
+            free(curr); //free memory
+            return;
+        }
+        prev = curr; //increment to look at the next item in the list
+        curr = curr->next;
+    }
+    return;
 }
-
-
