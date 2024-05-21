3cd0077ce61f04c359e0debe39fcc62f18781f20
Max Ratcliff <mwratcli@ucsc.edu>
2024-05-21 06:37:41 +0000

    fix leak

--- asgn6/hash.c
+++ asgn6/hash.c
@@ -25,12 +25,11 @@ bool hash_put(Hashtable *hashtable, char *key, int val) {
 
     size_t bucket_to_put = hash(key) % BUCKETS; // finds bucket based on hash function
     //create item to put
-    item *to_put = (item *) malloc(sizeof(item));
-    to_put->id = val;
-    strcpy(to_put->key, key);
-
-    return list_add(hashtable->bucket[bucket_to_put],
-        to_put); // attempt to add an item to the list at selected bucket
+    item to_put;
+    to_put.id = val;
+    strcpy(to_put.key, key);
+    // attempt to add an item to the list at selected bucket
+    return list_add(hashtable->bucket[bucket_to_put], &to_put);
 }
 
 int *hash_get(Hashtable *hashtable, char *key) {
@@ -39,13 +38,12 @@ int *hash_get(Hashtable *hashtable, char *key) {
     } //ensure both hashtable and key point somewhere
     size_t bucket_to_get = hash(key) % BUCKETS; // finds bucket based on hash function
 
-    item *temp = (item *) malloc(sizeof(item)); //list_find takes ptr to an item to find
-    strcpy(temp->key, key); //set of ket of item to find to key for hash to find
-    item *get_item
-        = list_find(hashtable->bucket[bucket_to_get], cmp, temp); //run list find to find item
-    return get_item
-               ? &(get_item->id)
-               : NULL; //return ptr to value of the iteam if get_item != NULL otherwise return NULL
+    item temp;
+    strcpy(temp.key, key); //set of ket of item to find to key for hash to find
+        //run list find to find item
+    item *get_item = list_find(hashtable->bucket[bucket_to_get], cmp, &temp);
+    //return ptr to value of the iteam if get_item != NULL otherwise return NULL
+    return get_item ? &(get_item->id) : NULL;
 }
 
 void hash_destroy(Hashtable **hashtable) {
